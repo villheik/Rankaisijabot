e7e0016b3b1a50a25718ae4f93d8a371ce5843e2
Ville Heikkinen <villeheikkinen92@gmail.com>
2021-02-06 17:44:21 +0200

    Read config file from multiple places

--- bot/bot.py
+++ bot/bot.py
@@ -3,10 +3,14 @@ from discord.ext import commands
 from os import path
 
 config = configparser.ConfigParser()
-if not (path.exists('../config.ini')):
-    print("Could not find config file.")
-config.read_file(open("../config.ini", "r"))
-TOKEN = config['BOT']['Token']
+
+# Try to read the config file from different places depending on where the script was ran from
+config.read("../config.ini")
+config.read("./config.ini")
+try:
+    TOKEN = config['BOT']['Token']
+except KeyError:
+    print("Could not find config.ini")
 
 description = '''Rankaisijabot'''
 bot = commands.Bot(command_prefix='!', description=description)
