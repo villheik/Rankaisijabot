2a7bb5e5902ed6d1cc70bf0d51982cb12939f6bd
Ville Heikkinen <villeheikkinen92@gmail.com>
2021-02-10 12:54:10 +0200

    Expand rankaisumetodit

--- bot/cogs/rankaisumetodit.py
+++ bot/cogs/rankaisumetodit.py
@@ -8,7 +8,9 @@ class Rankaisumetodit(commands.Cog, name="rankaisumetodit"):
     @commands.command(name="rankaise")
     async def rankaise(self, ctx, rankaisukohde=None):
         if (rankaisukohde == None):
-            await ctx.send("Rankaisen itseäni")
+            await ctx.send(f"{ctx.author.name} alensi itsensä pojaksi ja käytti rankaisumetodeja itseensä")
+        elif (len(rankaisukohde) > 20):
+            await ctx.send(f"{ctx.author.name} ei ottanut rankaisumetodeja vakavasti ja joutui poikien mukana kamarille")
         else:
             await ctx.send(f'{ctx.author.name} käytti rankaisumetodeja poikaan: {rankaisukohde}')
 
