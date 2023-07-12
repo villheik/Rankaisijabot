380f9aad4afb45d07a74bb45bc0bcb24027c802f
Ville Heikkinen <villeheikkinen92@gmail.com>
2023-07-12 18:42:17 +0300

    Review fixes:
    Renamed dice roll command to "dice"
    Added a roll command to roll between 1 and any number

--- bot/cogs/roller.py
+++ bot/cogs/roller.py
@@ -1,14 +1,18 @@
-import discord
 from discord.ext import commands
+from bot.constants import CustomException
+from discord.ext.commands.errors import BadArgument
+
 import random
 
 class Roller(commands.Cog, name="roll"):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="roll")
-    async def roller(self, ctx, *, roll_string:str):
+    @commands.command(name="dice")
+    async def dice(self, ctx, roll_string:str = None):
         try:
+            if (roll_string == None): 
+                raise CustomException
             dice, modifier = (roll_string.split('+') + ['0'])[:2]
             dice_count, dice_faces = map(int, dice.split('d'))
             modifier = int(modifier)
@@ -17,13 +21,24 @@ class Roller(commands.Cog, name="roll"):
             else:
                 rolls = [random.randint(1, dice_faces) for _ in range (dice_count)]
                 result = sum(rolls) + modifier
-                rolls_str = ','.join(str(roll) for roll in rolls)
-                await ctx.send(f'Rollasit {rolls_str}. Loppusumma {result}')
-        except ValueError:
+                rolls_str = ', '.join(str(roll) for roll in rolls)
+                await ctx.send(f'Rollasit {rolls_str}. Loppusumma {result}.')
+        except (ValueError,  CustomException) as e:
             await ctx.send("Syötä komento muodossa NdN+N tai NdN (eli vaikka 1d20+3 tai 5d100)")
 
     @commands.command(name="coinflip")
     async def coinflip(self, ctx):
         await ctx.send(f'Coinflip: {random.choice(["Kruunu", "Klaava"])}')
-def setup(bot):                                                                 
-    bot.add_cog(Roller(bot))    
+
+    @commands.command(name="roll")
+    async def roll(self, ctx, rollRangeEnd=None):
+        if (rollRangeEnd == None):
+            await ctx.send(random.randint(1, 100))
+        else:
+            try: 
+                await ctx.send(random.randint(1, int(rollRangeEnd)))
+            except:
+                await ctx.send("Syötä komento muodossa !roll <luku>")
+
+async def setup(bot):                                                                 
+    await bot.add_cog(Roller(bot))    
