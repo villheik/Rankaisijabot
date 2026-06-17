65805d7442c8ab76896e1d2593c8b285ab781514
Ville Heikkinen <villeheikkinen92@gmail.com>
2026-06-17 20:19:41 +0300

    Re-enable test_output to reject verbatim sentences

--- bot/cogs/markov.py
+++ bot/cogs/markov.py
@@ -144,7 +144,7 @@ class Markov(commands.Cog, name="markov"):
         text = "\n".join(messages)
         loop = context.bot.loop
         model = await loop.run_in_executor(None, lambda: markovify.NewlineText(text, state_size=state_size))
-        result = await loop.run_in_executor(None, lambda: model.make_sentence(tries=100, test_output=False, min_words=6))
+        result = await loop.run_in_executor(None, lambda: model.make_sentence(tries=100, min_words=6))
 
         if result is None:
             await context.send(f"Ei pystytty generoimaan tekstiä kohteelle `{target}`.")
